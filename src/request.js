c068050c14c3e27b7082f0f05fe5443d321923d8
hauleduc2404 <hauleduc2404@gmail.com>
2023-07-20 20:51:55 +0700

    update

--- src/request.js
+++ src/request.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const request = axios.create(
     {
         baseURL: process.env.REACT_APP_BASE_API,
-        timeout: 1000,
+        timeout: 5000,
         headers: {'X-Custom-Header': 'foobar'}
     }
 )
